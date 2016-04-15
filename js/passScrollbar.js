56f7b3799b755a2e3097388a524baaa109d2beab
Katie <kaspii@users.noreply.github.com>
2016-04-15 14:10:15 -0700

    Fixed drawing location of markers, bezier curve and endpoint appearance

--- js/passScrollbar.js
+++ js/passScrollbar.js
@@ -1,5 +1,8 @@
-$(document).ready(function() {
+var connections = new Array;
+var isOutlier = new Array; // boolean array for whether each receive point in the data is an outlier 
+var nConnections = 0;
 
+$(document).ready(function() {
 	var scrollbar = $("#passScrollbar");
 	var gamePasses = virginia;
 	// nPasses = gamePasses.length;
@@ -24,28 +27,85 @@ $(document).ready(function() {
 		scrollbar.append("Distance Of Pass: " + absoluteDistance + " yards<br>");
 		scrollbar.append("<\p>");
 		
-
 		// Create divs for start and end point nodes
 		xstart = xstart * .8;
 		xend = xend * .8;
-		ystart = ystart * 0.8;
-		yend = yend * 0.8;
+		ystart = 32 - ystart * .8;
+		yend = 32 - yend * .8;
 
-		$(".flex-container").append("<p id=\"end" + i + "\" style=\"color:rgb(255, 0, 0); position:absolute; z-index:10; top:" + yend + "vw; left:" + xend + "vw;\">X</p>");
-		$(".flex-container").append("<p id=\"start" + i + "\" style=\"color:rgb(0, 255, 0); position:absolute; z-index:10; top:" + ystart + "vw; left:" + xstart + "vw;\">$</p>");
+		// Draw startpoint (pass location) on screen
+		$(".flex-container").append("<p id=\"start" + i + "\" class=\"flex-item\" style=\"color:rgb(0, 255, 0); position:absolute; z-index:10; top:" + ystart + "vw; left:" + xstart + "vw;\">$</p>");
+
+		// Draw endpoint (receive location) on screen
+		if (yend <= 0)
+		{
+			// treat endpoint as outlier
+			isOutlier.push(true);
+		}
+		else
+		{
+			isOutlier.push(false);
+			$(".flex-container").append("<p id=\"end" + i + "\" style=\"color:rgb(255, 0, 0); position:absolute; z-index:10; top:" + yend + "vw; left:" + xend + "vw;\">X</p>");
+		}
+		
 	}
+
+	$(".flex-container").append("<p id=\"end" + i + "\" style=\"color:rgb(255, 0, 255); position:absolute; z-index:10; top: 32vw; left:20vw;\">&&&</p>");
+
 });
+
+var drawConnections = function () {
+	for (var i = 0; i < len; i++)
+	{
+		if (!isOutlier[i])
+		{
+			var stdiv = "start" + i;
+			var endiv = "end" + i;
+
+			var conn = jsPlumb.connect({
+				source:stdiv,
+				target:endiv,
+				connector:["Bezier", {curviness: 40}],
+				endpoint:"Blank"
+			});
+
+			nConnections++;
+			connections.push(conn);
+		}
+	}
+};
 
 jsPlumb.ready(function() {
-	for (var i = 0; i < 5; i++)
-	{
-		var stdiv = "start" + i;
-		var endiv = "end" + i;
+	establishedConnections = true;
 
-		jsPlumb.connect({
-			source:stdiv,
-			target:endiv,
-			endpoint:"Rectangle"
-		});
-	}
+	// for (var i = 0; i < len; i++)
+	// {
+	// 	if (!isOutlier[i])
+	// 	{
+	// 		var stdiv = "start" + i;
+	// 		var endiv = "end" + i;
+
+	// 		var conn = jsPlumb.connect({
+	// 			source:stdiv,
+	// 			target:endiv,
+	// 			connector:["Bezier", {curviness: 40}],
+	// 			endpoint:"Blank"
+	// 		});
+
+	// 		nConnections++;
+	// 		connections.push(conn);
+	// 	}
+	// }
+
+	drawConnections();
+
+	$(window).resize(function() {
+		console.log("Resizing window");
+
+		if(establishedConnections)
+		{
+			jsPlumb.detachEveryConnection();
+		}
+	});
 });
+
