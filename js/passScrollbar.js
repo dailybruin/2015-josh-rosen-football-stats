f4b0dafb3d7804d21797c9b14006b49d03852da0
Andy Shih <andyshih12@hotmail.com>
2016-04-18 13:22:19 -0700

    filter bar logic

--- js/passScrollbar.js
+++ js/passScrollbar.js
@@ -7,9 +7,19 @@ $(document).ready(function() {
 
 	for (var i = 0, len = gamePasses.length; i < len; i++) {
 		var pass = gamePasses[i];
-		scrollbar.append("<p>");
+		var p = $('<p class="pass"></p>');
+		
+		//initialize data-attrs for filter
+		if (pass.hasOwnProperty('Down')) {
+			p.attr("down", pass["Down"]);
+		}
+		if (pass.hasOwnProperty('Qtr.')) {
+			p.attr("quarter", pass["Qtr."]);
+		}
+		p.attr("PassNumber", i);
+		
 		for (var key in pass) {
-			scrollbar.append(key + ": " + pass[key] + "<br>");
+			p.append(key + ": " + pass[key] + "<br>");
 		}
 		
 		//Calculate absolute distance of pass
@@ -17,8 +27,39 @@ $(document).ready(function() {
 		var xend = pass["X-axis-receive"], yend = pass["X-axis-receive"];
 		var absoluteDistance = Math.sqrt(Math.pow(xend-xstart,2) + Math.pow(yend-ystart,2));
 		absoluteDistance = Math.round(absoluteDistance*100)/100;
-		scrollbar.append("Distance Of Pass: " + absoluteDistance + " yards<br>");
+		
+		p.append("Distance Of Pass: " + absoluteDistance + " yards" + "<br>");
 				
-		scrollbar.append("<\p>");
+		scrollbar.append(p);
 	}
+});
+
+
+$('#filter').click(function() {
+	var scrollbar = $("#passScrollbar");
+	
+	$('.pass').each(function(i, obj) {
+		
+		$(obj).show();
+		
+		//check downs
+		var downFilter = $('#down' + obj.getAttribute("down"));
+		if (!downFilter.is(':checked')) {
+			$(obj).hide();
+		}
+		
+		//check quarters
+		var quarterFilter = $('#quarter' + obj.getAttribute("quarter"));
+		if (!quarterFilter.is(':checked')) {
+			$(obj).hide();
+		}
+		
+		
+		
+	});
+	
+	
+	
+	
+	
 });
