2306948e1991bd40b6b19f7aa267941c6f2cd154
Catherine Truong <catherinetruongg@gmail.com>
2016-05-19 16:25:02 -0700

    Updated Statsbox; plays don't seem to be working rn for some reason

--- js/statsBox.js
+++ js/statsBox.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+	initializeGame();
+});
+
+function initializeGame() {
 	var game = virginia;
 
 	var completePasses = 0;
@@ -52,8 +56,8 @@ $(document).ready(function() {
 	document.getElementById('totalPasses').innerHTML=totalPasses;
 	document.getElementById('extrIncomplete').innerHTML=extraneous;
 	document.getElementById('adjCompletion').innerHTML=adjCompletionPercentage + '%';
-	document.getElementById('tpDistance').innerHTML=tpassDistance ;
-	document.getElementById('pDistance').innerHTML=avgPassDistance ;
+	document.getElementById('tpDistance').innerHTML=tpassDistance;
+	document.getElementById('pDistance').innerHTML=avgPassDistance;
 
 	var frequency = {};
 	var max = 0;
@@ -78,4 +82,218 @@ $(document).ready(function() {
 	maxReceiversString += maxReceivers[maxReceivers.length-1];
 
 	document.getElementById('receiver').innerHTML=maxReceiversString;
+
+}
+
+$('#quarterDownFilter').change(function() {
+	filterStatsBox();
 });
+
+$('#playTypeFilter').change(function() {
+	filterStatsBox();
+});
+
+function filterStatsBox() {
+	var qDFilter = $("#quarterDownFilter");
+	var qDSelectedId = qDFilter.find(":selected")[0].id;
+	var qDSelectedVal = qDFilter.find(":selected").val();
+	var filtersDown = (qDSelectedId.substring(0,4) == "down");	//check if filters quarter or down
+
+	if (qDSelectedVal !== "" && qDSelectedVal !== "all") {
+		if (!filtersDown) {
+			filterByQuarter(qDSelectedVal);
+		} else {
+			filterByDown(qDSelectedVal);
+		}
+	} else {
+		initializeGame();
+	}
+}
+
+function filterByQuarter(value) {
+	var game = virginia;
+
+	var completePasses = 0;
+	var totalPasses = 0;
+	var extraneous = 0;
+	var passDistance = 0;
+	var truePassDistance = 0;
+	var receivers = [];
+	var drops = 0;
+
+	for (var i = 0; i < game.length; i++) {
+		var pass = game[i];
+		if (pass['Qtr.'] == value) {
+			if (pass['Result of pass'] == 'Complete') {
+				completePasses += 1;
+				totalPasses += 1;
+			} else if (pass['Result of pass'] == 'Incomplete') {
+				totalPasses += 1;
+			}
+
+			if (pass.hasOwnProperty('Extraneous Incompletions')) {
+				if (pass['Extraneous Incompletions'] != 0) 
+					extraneous += 1;
+				else if (pass['Extraneous Incompletions'] === 'Drop')
+					drops += 1;
+			}
+
+			if (pass.hasOwnProperty('Yards') && pass.hasOwnProperty('YAC')) {
+				passDistance += (pass['Yards'] - pass['YAC'])
+			}
+
+			if (pass.hasOwnProperty('Pass X') && pass.hasOwnProperty('Pass Y') && pass.hasOwnProperty('Receive X') && pass.hasOwnProperty('Receive Y')) {
+				truePassDistance += Math.sqrt((Math.pow(pass['Pass X'],2)+(Math.pow(pass['Receive X'],2))) + (Math.pow(pass['Pass Y'],2) + (Math.pow(pass['Receive Y'],2))));
+			}
+
+			if (pass.hasOwnProperty('Receiver')) {
+				if (pass['Receiver'] != 0) {
+					receivers.push(pass['Receiver']);
+				}
+			}
+		}
+	}
+
+	var passPercentage = Math.round(completePasses/totalPasses * 100);
+	var avgPassDistance = Math.round(passDistance/totalPasses);
+	var adjCompletionPercentage = Math.round((completePasses + drops)/(totalPasses - extraneous - drops) * 100);
+	var tpassDistance = Math.round(truePassDistance/totalPasses);
+
+	document.getElementById('completePasses').innerHTML=passPercentage+'%';
+	document.getElementById('extrIncomplete').innerHTML=extraneous;
+	document.getElementById('totalPasses').innerHTML=totalPasses;
+	document.getElementById('pDistance').innerHTML=avgPassDistance;
+	document.getElementById('adjCompletion').innerHTML=adjCompletionPercentage + '%';
+	document.getElementById('tpDistance').innerHTML=tpassDistance;
+
+	var frequency = {};
+	var max = 0;
+	var result;
+	var maxReceivers = [];
+	for (var v in receivers) {
+		frequency[receivers[v]]=(frequency[receivers[v]] || 0)+1;
+		if (frequency[receivers[v]] > max) {
+			max = frequency[receivers[v]];
+		}
+	}
+	for (var v in frequency) {
+		if (frequency[v] === max) {
+			maxReceivers.push(v);
+		}
+	}
+	var maxReceiversString = "";
+	for (var v = 0; v < maxReceivers.length-1; v++) {
+		maxReceiversString += maxReceivers[v];
+		maxReceiversString += ", "
+	}
+	maxReceiversString += maxReceivers[maxReceivers.length-1];
+
+	document.getElementById('receiver').innerHTML=maxReceiversString;
+}
+
+function filterByDown(value) {
+	var game = virginia;
+
+	var completePasses = 0;
+	var totalPasses = 0;
+	var extraneous = 0;
+	var passDistance = 0;
+	var truePassDistance = 0;
+	var receivers = [];
+	var drops = 0;
+
+	for (var i = 0; i < game.length; i++) {
+		var pass = game[i];
+		if (pass['Down'] == value) {
+			if (pass['Result of pass'] == 'Complete') {
+				completePasses += 1;
+				totalPasses += 1;
+			} else if (pass['Result of pass'] == 'Incomplete') {
+				totalPasses += 1;
+			}
+
+			if (pass.hasOwnProperty('Extraneous Incompletions')) {
+				if (pass['Extraneous Incompletions'] != 0) 
+					extraneous += 1;
+				else if (pass['Extraneous Incompletions'] === 'Drop')
+					drops += 1;
+			}
+
+			if (pass.hasOwnProperty('Yards') && pass.hasOwnProperty('YAC')) {
+				passDistance += (pass['Yards'] - pass['YAC'])
+			}
+
+			if (pass.hasOwnProperty('Pass X') && pass.hasOwnProperty('Pass Y') && pass.hasOwnProperty('Receive X') && pass.hasOwnProperty('Receive Y')) {
+				truePassDistance += Math.sqrt((Math.pow(pass['Pass X'],2)+(Math.pow(pass['Receive X'],2))) + (Math.pow(pass['Pass Y'],2) + (Math.pow(pass['Receive Y'],2))));
+			}
+
+			if (pass.hasOwnProperty('Receiver')) {
+				if (pass['Receiver'] != 0) {
+					receivers.push(pass['Receiver']);
+				}
+			}
+		}
+	}
+
+	var passPercentage = Math.round(completePasses/totalPasses * 100);
+	var avgPassDistance = Math.round(passDistance/totalPasses);
+	var adjCompletionPercentage = Math.round((completePasses + drops)/(totalPasses - extraneous - drops) * 100);
+	var tpassDistance = Math.round(truePassDistance/totalPasses);
+
+	document.getElementById('completePasses').innerHTML=passPercentage+'%';
+	document.getElementById('extrIncomplete').innerHTML=extraneous;
+	document.getElementById('totalPasses').innerHTML=totalPasses;
+	document.getElementById('pDistance').innerHTML=avgPassDistance;
+	document.getElementById('adjCompletion').innerHTML=adjCompletionPercentage + '%';
+	document.getElementById('tpDistance').innerHTML=tpassDistance;
+
+	var frequency = {};
+	var max = 0;
+	var result;
+	var maxReceivers = [];
+	for (var v in receivers) {
+		frequency[receivers[v]]=(frequency[receivers[v]] || 0)+1;
+		if (frequency[receivers[v]] > max) {
+			max = frequency[receivers[v]];
+		}
+	}
+	for (var v in frequency) {
+		if (frequency[v] === max) {
+			maxReceivers.push(v);
+		}
+	}
+	var maxReceiversString = "";
+	for (var v = 0; v < maxReceivers.length-1; v++) {
+		maxReceiversString += maxReceivers[v];
+		maxReceiversString += ", "
+	}
+	maxReceiversString += maxReceivers[maxReceivers.length-1];
+
+	document.getElementById('receiver').innerHTML=maxReceiversString;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
